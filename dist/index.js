da7302ddb68f28197175c0d8bca34e553eb7212e
sergioamado <sergio.santos@dcomp.ufs.br>
2024-05-02 10:07:00 -0300

    Biblioteca externa validator

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const Operacoes_1 = require("./Operacoes");
 //import * as Operacoes from './Operacoes';
+const validator_1 = __importDefault(require("validator"));
 //import Operacoes from './Operacoes';  //importação por default
 //Operacoes.somar;
-let n1 = 10;
-let n2 = 2;
-console.log(`Versão: ${Operacoes_1.versao}`);
-console.log(`SOMA: ${(0, Operacoes_1.somar)(n1, n2)} `);
-console.log(`SUBTRAÇÃO: ${(0, Operacoes_1.subtrair)(n1, n2)} `);
-console.log(`MULTIPLICAÇÃO: ${(0, Operacoes_1.multiplicar)(n1, n2)}`);
+// let n1: number = 10;
+// let n2: number = 2;
+// console.log(`Versão: ${versao}`);
+// console.log(`SOMA: ${somar(n1,n2)} `);
+// console.log(`SUBTRAÇÃO: ${subtrair(n1,n2)} `);
+// console.log(`MULTIPLICAÇÃO: ${multiplicar(n1,n2)}`);
+console.log(validator_1.default.isEmail('suportecosup@gmail.com'));
+let ip = '127.0.0.1';
+console.log(validator_1.default.isIP(ip));
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import {versao, somar, subtrair, multiplicar} from './Operacoes';
 //import * as Operacoes from './Operacoes';
 
+import validator from 'validator';
+
 
 //import Operacoes from './Operacoes';  //importação por default
 //Operacoes.somar;
 
 
-let n1: number = 10;
-let n2: number = 2;
 
-console.log(`Versão: ${versao}`);
-console.log(`SOMA: ${somar(n1,n2)} `);
-console.log(`SUBTRAÇÃO: ${subtrair(n1,n2)} `);
-console.log(`MULTIPLICAÇÃO: ${multiplicar(n1,n2)}`);
+// let n1: number = 10;
+// let n2: number = 2;
 
+// console.log(`Versão: ${versao}`);
+// console.log(`SOMA: ${somar(n1,n2)} `);
+// console.log(`SUBTRAÇÃO: ${subtrair(n1,n2)} `);
+// console.log(`MULTIPLICAÇÃO: ${multiplicar(n1,n2)}`);
 
+console.log( validator.isEmail('suportecosup@gmail.com'));
+let ip = '127.0.0.1';
+console.log(validator.isIP(ip));
